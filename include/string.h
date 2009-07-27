f8c25e69ba4b1c37050924c70a6ab051af996313
Maks Verver <maksverver@geocities.com>
2009-07-27 02:17:07 +0200

    Implemented a bunch of string.h functions in assembly.

--- include/string.h
+++ include/string.h
@@ -3,23 +3,28 @@
 
 #include "stdlib.h"
 
-extern void *memcpy(void *dest, const void *src, size_t n);
+/* Implemented: */
+#define memcpy(dest, src, n) memmove(dest, src, n)
 extern void *memmove(void *dest, const void *src, size_t n);
 extern void *memchr(const void *s, char c, size_t n);
 extern int memcmp(const void *s1, const void *s2, size_t n);
 extern void *memset(void *s, int c, size_t n);
-extern char *strcat(char *dest, const char *src);
-extern char *strncat(char *dest, const char *src, size_t n);
+
+extern char *strcpy(char *dest, const char *src);
+extern char *strncpy(char *dest, const char *src, size_t n);
 extern char *strchr(const char *s, int c);
-extern char *strrchr(const char *s, int c);
+extern char *strcat(char *dest, const char *src);
+extern size_t strlen(const char *s);
 extern int strcmp(const char *s1, const char *s2);
 extern int strncmp(const char *s1, const char *s2, size_t n);
-extern char *strcpy(char *dest, const char *src);
-extern size_t strlen(const char *s);
 
+/* Unimplemented: (TODO!) */
+extern char *strncat(char *dest, const char *src, size_t n);
+extern char *strrchr(const char *s, int c);
+
+/* the following functions have low priority: */
 /*
 extern int strcoll(const char *, const char *);
-extern char *strncpy(char *dest, const char *src, size_t n);
 extern size_t strspn(const char *s, const char *accept);
 extern size_t strcspn(const char *s, const char *reject);
 extern char *strpbrk(const char *s, const char *accept);
